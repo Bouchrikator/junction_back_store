07e43d5667c54a6c1b9258dd1dcce6e77b76c4f1
Bouchrikator <62144269+Bouchrikator@users.noreply.github.com>
2021-12-17 02:54:58 +0000

    Update products.js

--- models/products.js
+++ models/products.js
@@ -1,13 +1,10 @@
 import mongoose from "mongoose";
 const required = true;
 const productSchema = new mongoose.Schema({
-    brand: { type: String, required },
-    category: { type: String, required },
-    name: { type: String, required },
-    description: { type: String, required },
-    price: { type: Number, required },
-    barCode: { type: String, required },
-    quantity: { type: Number, required },
-    pictutre: { type: String, required },
+    food_name: { type: String, required },
+    type: { type: String, required },
+    list_sauce: [{ type: String, required }],
+    
+    
 });
 export default mongoose.model("product", productSchema);
